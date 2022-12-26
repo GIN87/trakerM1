3e1fc9294357e32741f679584ca99dcfada27121
SOOJIN HA <peroxideha@gmail.com>
2022-12-26 23:23:13 +0900

    Organising

--- App.js
+++ App.js
@@ -1,99 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useLayoutEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, FlatList } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity, ScrollView, SafeAreaView, FlatList } from 'react-native';
 import { Feather, MaterialIcons } from '@expo/vector-icons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { currDate, DATA, saveData } from './src/data/data'
+import { Task } from './src/components/Task'
+import { renderWeeklyCal } from './src/components/WeeklyCal'
 
-const curr = new Date; // get current date
-const currDate = curr.getDate();
-const first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-console.log(curr.getDate());
-
-const DATA = [
-  {
-    day: 'SUN',
-    date: new Date(curr.setDate(first+0)).getDate(),
-  },
-  {
-    day: 'MON',
-    date: new Date(curr.setDate(first+1)).getDate(),
-  },
-  {
-    day: 'TUE',
-    date: new Date(curr.setDate(first+2)).getDate(),
-  },
-  {
-    day: 'WED',
-    date: new Date(curr.setDate(first+3)).getDate(),
-  },
-  {
-    day: 'THU',
-    date: new Date(curr.setDate(first+4)).getDate(),
-  },
-  {
-    day: 'FRI',
-    date: new Date(curr.setDate(first+5)).getDate(),
-  },
-  {
-    day: 'SAT',
-    date: new Date(curr.setDate(first+6)).getDate(),
-  },
-];
-
-const TICKLE = [
-  {
-    day: 'SUN',
-    isFilled: false,
-  },
-  {
-    day: 'MON',
-    isFilled: false,
-  },
-  {
-    day: 'TUE',
-    isFilled: false,
-  },
-  {
-    day: 'WED',
-    isFilled: false,
-  },
-  {
-    day: 'THU',
-    isFilled: false,
-  },
-  {
-    day: 'FRI',
-    isFilled: false,
-  },
-  {
-    day: 'SAT',
-    isFilled: false,
-  },
-]; 
-
-// component 공부하기
-const Item = ({ day, date }) => (
-  <View style={styles.item}>
-    <Text style={styles.day}>{day}</Text>
-    <View style={{...styles.highlight, backgroundColor: (currDate === date) ? 'lightgray' : null}}>
-      <Text style={styles.dates}>{date}</Text>
-    </View>
-  </View>
-);
-
-const Tickle = ({ }) => (
-  <View style={styles.tickle}>
-  </View>
-);
 
 export default function App() {
 
-  const renderItem = ({ item }) => ( // doc에 renderItem은 {item, index, separators} 쓰도록 정의되어 있음. DATA array의 각 오브젝트가 item.  __ 어떤 오브젝트가 함수의 parameter로 들어왔을때 item 이라는 key에 매칭되는 값을 인자로 넘김 (Destructuring assignment 참조)
-    <Item day={item.day} date={item.date} /> 
-  );
+  const STORAGE_KEY = "@tickle";
+  const [data, setData] = useState(null);
 
-  const renderTickle = ({ item }) => (
-    <Tickle />
-  );
+  const loadData = async (KEY) => {
+    const s = await AsyncStorage.getItem(KEY);
+    setData(JSON.parse(s));
+  }
+
+  const updateData = async (data, id, isFilled) => { 
+    const temp = [...data];
+    temp.find(e => e.id === id).isFilled = !isFilled;
+    await setData(temp);
+    saveData(STORAGE_KEY, temp);
+  }
+
+  useEffect( () => {
+    loadData(STORAGE_KEY);    
+  }, []);
+
+  if (data === null) {
+    setData(DATA);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -105,21 +43,12 @@ export default function App() {
       <View style={styles.weeklyCal}>
         <FlatList
           data={DATA}
-          renderItem={renderItem}
-          // keyExtractor={item => item.key}
+          renderItem={renderWeeklyCal}
           horizontal={true}
           contentContainerStyle={{ flex: 1, justifyContent: "space-between"}}
         />
       </View>
-      <View style={styles.task}>
-      <FlatList
-          data={TICKLE}
-          renderItem={renderTickle}
-          horizontal={true}
-          contentContainerStyle={{ flex: 1, justifyContent: "space-between"}}
-        />
-      </View>
-
+      <Task module={"One time complete"} taskName={"Probiotics"} desc={"Once a day"} data={data} updateData={updateData}></Task>
     </SafeAreaView>
   );
 }
@@ -149,39 +78,4 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: 'lightgrey', 
   },
-  item: {
-    // backgroundColor: "orange",
-    alignItems: 'center',
-    width: 40 // fixed width due to different box sizes for each day(text)
-  },
-  day: {
-    fontSize: 14,
-    paddingBottom: 6,
-  },
-  dates: {
-    fontSize: 20,
-    fontWeight: '400',
-  },
-  highlight: {
-    width : '100%',
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-  },
-  task: {
-    marginTop: 20,  // ** temporary
-    paddingHorizontal: 15,
-    // backgroundColor: "orange",
-    width : "100%"
-  },
-  tickle: {
-    borderRadius: 50,
-    width : 40,
-    height : 40,
-    backgroundColor: "green",
-    alignItems: 'center',
-    justifyContent: 'center'
-    // width: '100%',
-  }
 });
